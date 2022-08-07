158f0560e366e804a34f57945c92b6f40f06ee91
Gabriel Sartori Rangel <gabriel.sartorirangel@gmail.com>
2022-08-07 19:35:08 -0300

    Div centralizada :D

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
+import MainBox from './components/MainBox';
+
 function App() {
   return (
-    <h1>Hello world</h1>
+    <MainBox />
   );
 }
 
---
+++ src/components/MainBox/index.tsx
@@ -0,0 +1,7 @@
+import './style.css';
+
+export default function MainBox() {
+  return (
+    <div>Hello World</div>
+  );
+}
